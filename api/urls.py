e5d9f65581f5d0068130a2e0e501ea4602900b6e
asanka-j <asanka.jsndr@gmail.com>
2024-01-04 21:56:35 +0530

    add test cases for cart functions

--- api/urls.py
+++ api/urls.py
@@ -1,16 +1,22 @@
 from django.urls import path, include
 from .views import (
-    ProductViewSet
+    ProductViewSet,CartViewSet
 )
 
 
 product_list = ProductViewSet.as_view({'get': 'list', 'post': 'create'})
 product_detail = ProductViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})
+cart_list = CartViewSet.as_view({'get': 'list'})
+cart_create = CartViewSet.as_view({'get':'get','post': 'create'})
 
 urlpatterns = [
     path('auth/', include('rest_framework.urls')),
     
     path('products/', product_list, name='product-list'),
     path('products/<int:pk>/', product_detail, name='product-detail'),
+    
+    path('cart/', cart_create, name='cart-create'), 
+    path('cart/list/', cart_list, name='cart-list'),
+
    
 ]
--- api/views.py
+++ api/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from rest_framework.viewsets import ViewSet, ModelViewSet
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Product#, Cart, CartItem, Order
-from .serializers import ProductSerializer#, CartSerializer, OrderSerializer, CartItemSerializer, OrderItemSerializer
+from .models import Product, Cart, CartItem, Order
+from .serializers import ProductSerializer, CartSerializer, OrderSerializer, CartItemSerializer, OrderItemSerializer
 from rest_framework.permissions import AllowAny
 
 class ProductViewSet(ModelViewSet):
@@ -11,3 +11,39 @@ class ProductViewSet(ModelViewSet):
     serializer_class = ProductSerializer
     permission_classes = [AllowAny]
 
+class CartViewSet(ModelViewSet):
+    queryset = Cart.objects.all()
+    serializer_class = CartSerializer
+    
+    def get(self, request, *args, **kwargs):
+        cart_id = request.query_params.get('cart_id')
+    
+        if cart_id: # If cart_id is provided
+            try:
+                cart = Cart.objects.get(cart_id=cart_id)
+                if cart.user:   # If the cart is associated with a user
+                    if cart.user == request.user: # If the cart is associated with the user
+                        serializer = self.get_serializer(cart)
+                        return Response(serializer.data, status=status.HTTP_200_OK)
+                else:   # If the cart is not associated with a user
+                    serializer = self.get_serializer(cart)
+                    return Response(serializer.data, status=status.HTTP_200_OK)
+                    
+                return Response({"message": "Unauthorized access to cart"}, status=status.HTTP_403_FORBIDDEN)
+            except Cart.DoesNotExist:
+                return Response({"message": "Cart not found"}, status=status.HTTP_404_NOT_FOUND)
+        
+        if request.user.is_authenticated:
+            existing_cart = Cart.objects.filter(user=request.user).first()
+            if existing_cart:
+                serializer = self.get_serializer(existing_cart)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+
+            cart = Cart.objects.create(user=request.user)
+            serializer = self.get_serializer(cart)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            cart = Cart.objects.create()
+            serializer = self.get_serializer(cart)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
