f34d59924fc468d39878d9c8e72d39fea22fe174
asanka-j <asanka.jsndr@gmail.com>
2024-01-04 21:54:29 +0530

    add models to django admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from .models import Product, Order, Cart, CartItem
+
+admin.site.register(Product)
+admin.site.register(Order)
+
+class CartItemInline(admin.TabularInline):
+    model = CartItem
+    extra = 1
+    
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    inlines = (CartItemInline,)
+    list_display = ('id','cart_id','user')
+    readonly_fields = ('cart_id', ) 
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ('cart', 'product', 'quantity')
